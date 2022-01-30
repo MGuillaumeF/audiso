2f9091879c9d6823177aee4ed597ac7245ce89f4
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-30 16:42:39 +0100

    Create entry-test.spec.ts

---
+++ test/entry-test.spec.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import { isAudit } from '../src/audiso.ts';
+import { promises } from 'fs';
+const { fs } = promises;
+
+// test configuration
+describe('entry test', () => {
+
+    console.log('entry test');
+
+    test('empty audit report', async () => {
+        expect(isAudit({})).toBeFalse();
+        expect(isAudit(null)).toBeFalse();
+        expect(isAudit(undefined)).toBeFalse();
+    });
+
+    test('audit first example', async () => {
+        const example1 = await fs.readFile(path.resolve(process.cwd(), './test/resources/audit-example-1.json'));
+        const example1Str = example.toString();
+        expect(isAudit(JSON.parse(example1Str))).toBeTrue();
+    });
+});
