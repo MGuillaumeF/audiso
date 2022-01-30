bef1b042008735d2ccb0208a7991cf944f37a40f
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-30 19:25:21 +0100

    Update output-test.spec.ts

--- test/output-test.spec.ts
+++ test/output-test.spec.ts
@@ -10,7 +10,7 @@ describe('output test', () => {
 
     test('audit first example output', async () => {
         await audiso.auditToSonar({
-            inputFile: path.resolve(process.cwd(), 'test/resources/audit-example-1.json'),
+            inputFilePath: path.resolve(process.cwd(), 'test/resources/audit-example-1.json'),
             outputFilePath: path.resolve(process.cwd(), 'test/resources/audit-example-output-1.json'),
             packageFilePath: path.resolve(process.cwd(), 'test/resources/package-example-1.json')
         });
