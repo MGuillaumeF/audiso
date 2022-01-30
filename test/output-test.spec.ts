baf4212fcf3ad1bd865a3ac864a6515e9af0ffc3
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-30 18:45:49 +0100

    Create output-test.spec.ts

---
+++ test/output-test.spec.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import audiso from '../src/audiso.ts';
+import { readParameters } from '../src/args/parameters/index.ts';
+
+// test output
+describe('output test', () => {
+
+    console.log('output test');
+
+    test('audit first example output', async () => {
+        await audiso({
+            inputFile: path.resolve(process.cwd(), 'test/resources/audit-example-1.json),
+            outputFilePath: path.resolve(process.cwd(), 'test/resources/audit-example-output-1.json),
+            packageFilePath: path.resolve(process.cwd(), 'test/resources/package-example-1.json)
+        });
+        const reportBuffer = await fs.readFile(path.resolve(process.cwd(), 'test/resources/audit-example-output-1.json));
+        const report = JSON.parse(reportBuffer.toString());
+        
+        expect(Array.isArray(report?.issues)).toBeTruthy();
+        expect(report?.issues?.length).toBe(2);
+    });
+});
