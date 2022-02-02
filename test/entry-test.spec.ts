d6299a877e13c975f764a02ee652ba0610d0e29d
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-02-02 22:26:50 +0100

    Update entry-test.spec.ts

--- test/entry-test.spec.ts
+++ test/entry-test.spec.ts
@@ -56,6 +56,6 @@ describe('entry test', () => {
         ]);
        
         expect(configuration).toBe('*** NOTICE : @mguillaumef/audiso v0.0.2\nThis module module convert npm-audit json report to sonarqube generic data issue report.\n[-p], [--package-file]         1   string   The path of package.json (default: ./package.json)\n[-o], [--output-file]          1   string   The output path of sonarqube issue report (default: ./audit-dependency-report-sonarqube.json)\n[-i], [--input-file]           1   string   The input path of npm-audit report (default: ./audit-dependency-report.json)');
-        expect((await getVersion()).toBe('v0.0.2');
+        expect((await getVersion())).toBe('v0.0.2');
     });
 });
