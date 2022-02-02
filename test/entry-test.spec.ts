39c9f45fbd739a6be261c267699fdf25d106e835
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-02-02 22:43:27 +0100

    Update entry-test.spec.ts

--- test/entry-test.spec.ts
+++ test/entry-test.spec.ts
@@ -2,6 +2,9 @@ import path from 'path';
 import { isAudit } from '../src/audiso.ts';
 import { getHelper, getVersion } from '../src/args/core/index.ts';
 import { promises as fs } from 'fs';
+import util from 'util';
+import { exec } from 'child_process';
+const cmd = util.promisify(exec);
 
 // test configuration
 describe('entry test', () => {
@@ -57,5 +60,16 @@ describe('entry test', () => {
        
         expect(configuration).toBe('*** NOTICE : @mguillaumef/audiso v0.0.2\nThis module module convert npm-audit json report to sonarqube generic data issue report.\n[-p], [--package-file]         1   string   The path of package.json (default: ./package.json)\n[-o], [--output-file]          1   string   The output path of sonarqube issue report (default: ./audit-dependency-report-sonarqube.json)\n[-i], [--input-file]           1   string   The input path of npm-audit report (default: ./audit-dependency-report.json)');
         expect((await getVersion())).toBe('v0.0.2');
+    });
+
+    test('cli tests helper/version', async () => {
+        try {
+            const helpCmd = await cmd(['audiso', '-h'].join(' '));
+            const versionCmd = await cmd(['audiso', '-v'].join(' '));
+            expect((versionCmd.stdout).toBe('v0.0.2');
+            expect(helpCmd.stdout).toBe('*** NOTICE : @mguillaumef/audiso v0.0.2\nThis module module convert npm-audit json report to sonarqube generic data issue report.\n[-p], [--package-file]         1   string   The path of package.json (default: ./package.json)\n[-o], [--output-file]          1   string   The output path of sonarqube issue report (default: ./audit-dependency-report-sonarqube.json)\n[-i], [--input-file]           1   string   The input path of npm-audit report (default: ./audit-dependency-report.json)');
+        } catch (e) {
+            console.error(e); // should contain code (exit code) and signal (that caused the termination).
+        }
     });
 });
