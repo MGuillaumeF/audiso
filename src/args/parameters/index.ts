bb822843d6ff53a59d4a3aa8de7903166c952791
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-02-02 21:18:07 +0100

    Update index.ts

--- src/args/parameters/index.ts
+++ src/args/parameters/index.ts
@@ -27,7 +27,7 @@ export function isKeyOfParameters(key : string) : key is KeyOfParameters {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
 export function isParameters(data: any): data is Parameters {
     return (
-        typeof data === "object" &&
+        data != null && typeof data === "object" &&
         [
             data?.inputFilePath,
             data?.outputFilePath,
