f2b64837cd2d5cf97395431cb26f1eb38aba4a1a
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-02-04 00:18:19 +0100

    Update types.ts

--- src/args/core/types.ts
+++ src/args/core/types.ts
@@ -1,7 +1,16 @@
+/**
+ * type of single arg of cli
+ */
 export type CliArgument = string | boolean | number;
 
+/**
+ * type of list args of cli
+ */
 export type CliArguments = string[] | boolean[] | number[];
 
+/**
+ * type of one argument of cli
+ */
 export type ConfigurationItem = {
     alias: string[];
     description: string;
