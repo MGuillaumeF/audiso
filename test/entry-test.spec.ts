30c792c95cfaee16a3798d3ccd5ae6e40d7b5085
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-02-03 06:42:53 +0100

    Update entry-test.spec.ts

--- test/entry-test.spec.ts
+++ test/entry-test.spec.ts
@@ -6,6 +6,8 @@ import util from 'util';
 import { exec } from 'child_process';
 const cmd = util.promisify(exec);
 
+const PACKAGE_VERSION = '0.0.3';
+
 // test configuration
 describe('entry test', () => {
 
@@ -59,14 +61,14 @@ describe('entry test', () => {
         ]);
        
         expect(configuration).toBe('*** NOTICE : @mguillaumef/audiso v0.0.2\nThis module module convert npm-audit json report to sonarqube generic data issue report.\n[-p], [--package-file]         1   string   The path of package.json (default: ./package.json)\n[-o], [--output-file]          1   string   The output path of sonarqube issue report (default: ./audit-dependency-report-sonarqube.json)\n[-i], [--input-file]           1   string   The input path of npm-audit report (default: ./audit-dependency-report.json)');
-        expect((await getVersion())).toBe('v0.0.2');
+        expect((await getVersion())).toBe(`v${PACKAGE_VERSION}`);
     });
 
     test('cli tests helper/version', async () => {
         try {
             const helpCmd = await cmd(['audiso', '-h'].join(' '));
             const versionCmd = await cmd(['audiso', '-v'].join(' '));
-            expect(versionCmd.stdout).toBe('v0.0.2');
+            expect(versionCmd.stdout).toBe(`v${PACKAGE_VERSION}`);
             expect(helpCmd.stdout).toBe('*** NOTICE : @mguillaumef/audiso v0.0.2\nThis module module convert npm-audit json report to sonarqube generic data issue report.\n[-p], [--package-file]         1   string   The path of package.json (default: ./package.json)\n[-o], [--output-file]          1   string   The output path of sonarqube issue report (default: ./audit-dependency-report-sonarqube.json)\n[-i], [--input-file]           1   string   The input path of npm-audit report (default: ./audit-dependency-report.json)');
         } catch (e) {
             console.error(e); // should contain code (exit code) and signal (that caused the termination).
