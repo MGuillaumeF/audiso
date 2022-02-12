58ee4372efd66018d7c4ff73410dd5fd9d2e5c67
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-02-12 04:16:20 +0100

    Mise en place du logger et formatage

--- src/audiso.ts
+++ src/audiso.ts
@@ -3,9 +3,9 @@
 // node imports
 import { promises as fs } from "fs";
 // cli args reading imports
-import { readParameters, Parameters } from './args/parameters/index';
+import { readParameters, Parameters } from "./args/parameters/index";
 // cli logger
-import { CliLogger, LoggerTheme } from './logger/core/index';
+import { CliLogger, LoggerTheme } from "./logger/core/index";
 
 // spaces of pretty print of JSON objects
 const JSON_LEFT_SPACE = 4;
@@ -18,51 +18,51 @@ const LOGGER = CliLogger.getInstance();
  * type of vulnerability object of audit report (input)
  */
 type Vulnerability = {
-    /**
-     * description of fix available or false if fix not available
-     */
-    fixAvailable: { name: string; version: string } | false;
-    /**
-     * the dependence with vulnerability is direct dependency declared in package json file. (false if sub-dependency)
-     */
-    isDirect: boolean;
-    /**
-     * the severity of vulnerability
-     */
-    severity: string;
+  /**
+   * description of fix available or false if fix not available
+   */
+  fixAvailable: { name: string; version: string } | false;
+  /**
+   * the dependence with vulnerability is direct dependency declared in package json file. (false if sub-dependency)
+   */
+  isDirect: boolean;
+  /**
+   * the severity of vulnerability
+   */
+  severity: string;
 };
 
 /**
  * type of audit report (input)
  */
 type Audit = {
+  /**
+   * the version of audit report
+   */
+  auditReportVersion: number;
+  /**
+   * metadata is the summary of result of report
+   */
+  metadata: {
     /**
-     * the version of audit report
-     */
-    auditReportVersion: number;
-    /**
-     * metadata is the summary of result of report
-     */
-    metadata: {
-        /**
-         * object with vulnerability count by severity
-         */
-        vulnerabilities: {
-            /**
-             * count item, key is severity level
-             */
-            [key: string]: number;
-        };
-    };
-    /**
-     * object of vulnerabilities, each entry is a vulnerability
+     * object with vulnerability count by severity
      */
     vulnerabilities: {
-        /**
-         * vulnerability item, key is package name of vulnerability
-         */
-        [key: string]: Vulnerability;
+      /**
+       * count item, key is severity level
+       */
+      [key: string]: number;
     };
+  };
+  /**
+   * object of vulnerabilities, each entry is a vulnerability
+   */
+  vulnerabilities: {
+    /**
+     * vulnerability item, key is package name of vulnerability
+     */
+    [key: string]: Vulnerability;
+  };
 };
 
 /**
@@ -72,32 +72,44 @@ type Audit = {
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
 export function isAudit(data: any): data is Audit {
-    let result = true;
-    // check auditReportVersion field
-    result &&= typeof data?.auditReportVersion === "number";
-    // check metadata field
-    result &&=
-        typeof data?.metadata?.vulnerabilities === "object" &&
-        Object.entries(data.metadata.vulnerabilities).every(([key, value]) => {
-            const isValid = typeof key === "string" && typeof value === "number";
-            if (!isValid) {
-                LOGGER.error(LoggerTheme.DATA, 'Invalid metadata of vulnerability object', Error(`Invalid metadata ${key}, name of metadata is typeof ${typeof key}, value is ${value} of type ${typeof value}`));
-            }
-            return isValid;
-        });
+  let result = true;
+  // check auditReportVersion field
+  result &&= typeof data?.auditReportVersion === "number";
+  // check metadata field
+  result &&=
+    typeof data?.metadata?.vulnerabilities === "object" &&
+    Object.entries(data.metadata.vulnerabilities).every(([key, value]) => {
+      const isValid = typeof key === "string" && typeof value === "number";
+      if (!isValid) {
+        LOGGER.error(
+          LoggerTheme.DATA,
+          "Invalid metadata of vulnerability object",
+          Error(
+            `Invalid metadata ${key}, name of metadata is typeof ${typeof key}, value is ${value} of type ${typeof value}`
+          )
+        );
+      }
+      return isValid;
+    });
 
-    result &&=
-        typeof data?.vulnerabilities === "object" &&
-        Object.entries(data.vulnerabilities).every(([key, value]) => {
-            const isVulnerabilityCheck = isVulnerability(value);
-            const isValid = typeof key === "string" && isVulnerabilityCheck;
-            if (!isValid) {
-                LOGGER.error(LoggerTheme.DATA, 'Invalid Audit Report', Error(`Invalid vulnerability ${key}, name of vulnerability is typeof ${typeof key}, vulnerability check result ${isVulnerabilityCheck}`));
-            }
-            return isValid;
-        });
+  result &&=
+    typeof data?.vulnerabilities === "object" &&
+    Object.entries(data.vulnerabilities).every(([key, value]) => {
+      const isVulnerabilityCheck = isVulnerability(value);
+      const isValid = typeof key === "string" && isVulnerabilityCheck;
+      if (!isValid) {
+        LOGGER.error(
+          LoggerTheme.DATA,
+          "Invalid Audit Report",
+          Error(
+            `Invalid vulnerability ${key}, name of vulnerability is typeof ${typeof key}, vulnerability check result ${isVulnerabilityCheck}`
+          )
+        );
+      }
+      return isValid;
+    });
 
-    return result;
+  return result;
 }
 
 /**
@@ -107,19 +119,26 @@ export function isAudit(data: any): data is Audit {
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
 function isVulnerability(data: any): data is Vulnerability {
-    let result = true;
-    result &&= typeof data?.fixAvailable === "boolean" ||
-        [data?.fixAvailable?.name, data?.fixAvailable?.version].every(
-            (value) => typeof value === "string"
-        );
-    result &&= typeof data?.isDirect === "boolean";
-    result &&= ["info", "low", "moderate", "high", "critical"].includes(
-        data?.severity
+  let result = true;
+  result &&=
+    typeof data?.fixAvailable === "boolean" ||
+    [data?.fixAvailable?.name, data?.fixAvailable?.version].every(
+      (value) => typeof value === "string"
     );
-    if (!result) {
-        LOGGER.error(LoggerTheme.DATA, 'Bad vulnerability object definition', Error(`is not a valid vulnerability object, data fixAvailable of ${data?.fixAvailable}, is direct ${data?.isDirect}, severity ${data?.severity}`));
-    }
-    return result;
+  result &&= typeof data?.isDirect === "boolean";
+  result &&= ["info", "low", "moderate", "high", "critical"].includes(
+    data?.severity
+  );
+  if (!result) {
+    LOGGER.error(
+      LoggerTheme.DATA,
+      "Bad vulnerability object definition",
+      Error(
+        `is not a valid vulnerability object, data fixAvailable of ${data?.fixAvailable}, is direct ${data?.isDirect}, severity ${data?.severity}`
+      )
+    );
+  }
+  return result;
 }
 
 /**
@@ -127,138 +146,158 @@ function isVulnerability(data: any): data is Vulnerability {
  * @param params Run parameters (entry, output, package)
  */
 async function auditToSonar(params: Parameters): Promise<void> {
-    // map to convert npm-audit severity to sonarqube severity
-    const npmSeverityToSonar = new Map([
-        ["info", "INFO"],
-        ["low", "MINOR"],
-        ["moderate", "MINOR"],
-        ["high", "MAJOR"],
-        ["critical", "CRITICAL"]
-    ]);
+  // map to convert npm-audit severity to sonarqube severity
+  const npmSeverityToSonar = new Map([
+    ["info", "INFO"],
+    ["low", "MINOR"],
+    ["moderate", "MINOR"],
+    ["high", "MAJOR"],
+    ["critical", "CRITICAL"]
+  ]);
 
-    let auditJsonString = "";
-    // read input file
-    try {
-        const buffer = await fs.readFile(params.inputFilePath);
-        auditJsonString = buffer.toString();
-    } catch (error) {
-        const rethrowError = Error("input file read failed");
-        LOGGER.error(LoggerTheme.DATA, "input file read failed", error instanceof Error ? error : rethrowError);
-        throw rethrowError;
-    }
+  let auditJsonString = "";
+  // read input file
+  try {
+    const buffer = await fs.readFile(params.inputFilePath);
+    auditJsonString = buffer.toString();
+  } catch (error) {
+    const rethrowError = Error("input file read failed");
+    LOGGER.error(
+      LoggerTheme.DATA,
+      "input file read failed",
+      error instanceof Error ? error : rethrowError
+    );
+    throw rethrowError;
+  }
 
-    let audit: Audit | null = null;
-    let data: unknown;
+  let audit: Audit | null = null;
+  let data: unknown;
 
-    // parsing of audit report
-    try {
-        data = JSON.parse(auditJsonString);
-    } catch (error) {
-        console.error("entry data invalid, parsing error", error);
-        throw Error("entry data invalid, parsing error");
-    }
-    // narrowing of data as audit object
-    if (isAudit(data)) {
-        audit = data;
-    } else {
-        throw Error("entry data is not a valid npm-audit data");
-    }
-    const issues = [];
-    // generate engine id for all issues
-    const engineId = `npm-audit-${audit.auditReportVersion}`;
-    // for each vulnerability object of audit report
-    for (const [packageName, vulnerability] of Object.entries(
-        audit.vulnerabilities
-    )) {
-        // filter direct vulnerability only
-        if (vulnerability.isDirect) {
-            const packageJsonFileBuffer = await fs.readFile(
-                params.packageFilePath
-            );
-            const packageJsonFile = packageJsonFileBuffer.toString();
-            const packageNameIndex = packageJsonFile.indexOf(packageName);
-            // search rows where package name appears in package.json file
-            const rows = packageJsonFile.slice(0, packageNameIndex).split("\n");
-            const startLine = rows.length;
-            // get column of package name in line on package.json
-            const startColumn = rows.slice(-1).shift()?.length || 0;
-            const endColumn = startColumn + packageName.length;
+  // parsing of audit report
+  try {
+    data = JSON.parse(auditJsonString);
+  } catch (error) {
+    const rethrowError = Error("entry data invalid, parsing error");
+    LOGGER.error(
+      LoggerTheme.DATA,
+      "entry data invalid, parsing error",
+      error instanceof Error ? error : rethrowError
+    );
+    throw rethrowError;
+  }
+  // narrowing of data as audit object
+  if (isAudit(data)) {
+    audit = data;
+  } else {
+    throw Error("entry data is not a valid npm-audit data");
+  }
+  const issues = [];
+  // generate engine id for all issues
+  const engineId = `npm-audit-${audit.auditReportVersion}`;
+  // for each vulnerability object of audit report
+  for (const [packageName, vulnerability] of Object.entries(
+    audit.vulnerabilities
+  )) {
+    // filter direct vulnerability only
+    if (vulnerability.isDirect) {
+      const packageJsonFileBuffer = await fs.readFile(params.packageFilePath);
+      const packageJsonFile = packageJsonFileBuffer.toString();
+      const packageNameIndex = packageJsonFile.indexOf(packageName);
+      // search rows where package name appears in package.json file
+      const rows = packageJsonFile.slice(0, packageNameIndex).split("\n");
+      const startLine = rows.length;
+      // get column of package name in line on package.json
+      const startColumn = rows.slice(-1).shift()?.length || 0;
+      const endColumn = startColumn + packageName.length;
 
-            let message = `The dependency ${packageName} has vulnerability`;
-            if (vulnerability.fixAvailable) {
-                message += `, fix available in ${vulnerability.fixAvailable.name} version : ${vulnerability.fixAvailable.version}`;
-            }
-            // add issue in sonarqube report array
-            issues.push({
-                engineId,
-                ruleId: "dependency-vulnerability",
-                severity: npmSeverityToSonar.get(vulnerability.severity),
-                type: "VULNERABILITY",
-                primaryLocation: {
-                    message,
-                    filePath: params.packageFilePath,
-                    textRange: {
-                        startLine,
-                        startColumn,
-                        endColumn
-                    }
-                }
-            });
-        }
-    }
-
-    // add summary metadata issue as INFO in at the top of package.json file
-    issues.push({
+      let message = `The dependency ${packageName} has vulnerability`;
+      if (vulnerability.fixAvailable) {
+        message += `, fix available in ${vulnerability.fixAvailable.name} version : ${vulnerability.fixAvailable.version}`;
+      }
+      // add issue in sonarqube report array
+      issues.push({
         engineId,
-        ruleId: "summary-dependency-vulnerability",
-        severity: "INFO",
+        ruleId: "dependency-vulnerability",
+        severity: npmSeverityToSonar.get(vulnerability.severity),
         type: "VULNERABILITY",
         primaryLocation: {
-            message: Object.entries(audit.metadata.vulnerabilities)
-                .map((entry) => {
-                    const [level, quantity] = entry;
-                    return `- ${quantity} ${level}`;
-                })
-                .join(" \r\n"),
-            filePath: params.packageFilePath,
-            textRange: {
-                startLine: 1,
-                startColumn: 0
-            }
+          message,
+          filePath: params.packageFilePath,
+          textRange: {
+            startLine,
+            startColumn,
+            endColumn
+          }
         }
-    });
-
-    // write sonarqube generic issue report
-    const output = JSON.stringify({ issues }, null, JSON_LEFT_SPACE);
-    console.debug("issues generated", output);
-    try {
-        await fs.writeFile(params.outputFilePath, output);
-    } catch (error) {
-        console.error("output file write failed", error);
-        throw Error('output error, report not writed');
+      });
     }
+  }
+
+  // add summary metadata issue as INFO in at the top of package.json file
+  issues.push({
+    engineId,
+    ruleId: "summary-dependency-vulnerability",
+    severity: "INFO",
+    type: "VULNERABILITY",
+    primaryLocation: {
+      message: Object.entries(audit.metadata.vulnerabilities)
+        .map((entry) => {
+          const [level, quantity] = entry;
+          return `- ${quantity} ${level}`;
+        })
+        .join(" \r\n"),
+      filePath: params.packageFilePath,
+      textRange: {
+        startLine: 1,
+        startColumn: 0
+      }
+    }
+  });
+
+  // write sonarqube generic issue report
+  const output = JSON.stringify({ issues }, null, JSON_LEFT_SPACE);
+
+  LOGGER.debug(LoggerTheme.DATA, "issues generated", output);
+  try {
+    await fs.writeFile(params.outputFilePath, output);
+  } catch (error) {
+    const rethrowError = Error("output error, report not writed");
+    LOGGER.error(
+      LoggerTheme.IO,
+      "output file write failed",
+      error instanceof Error ? error : rethrowError
+    );
+    throw rethrowError;
+  }
 }
 
 // if run with node command
 if (require.main === module) {
-    (async function () {
-        try {
-            // get arguments of process run
-            const args = process.argv.slice(CLI_ARGUMENT_PADDING);
+  (async function () {
+    try {
+      // get arguments of process run
+      const args = process.argv.slice(CLI_ARGUMENT_PADDING);
 
-            const params = await readParameters(args);
+      const params = await readParameters(args);
 
-            if (params !== null) {
-                auditToSonar(params);
-            } else {
-                console.error("parameters invalid");
-                throw Error("entry parameters invalid");
-            }
-        } catch (error) {
-            console.error("convert failed with error", error);
-            process.exit(1);
-        }
-    })();
+      if (params !== null) {
+        auditToSonar(params);
+      } else {
+        const error = Error("entry parameters invalid");
+        LOGGER.error(LoggerTheme.IO, "parameters invalid", error);
+        throw error;
+      }
+    } catch (error) {
+      const rethrowError = Error("convert failed with error");
+      LOGGER.error(
+        LoggerTheme.IO,
+        "convert failed with erro",
+        error instanceof Error ? error : rethrowError
+      );
+
+      process.exit(1);
+    }
+  })();
 }
 
 const audiso = { auditToSonar };
--- src/logger/core/index.ts
+++ src/logger/core/index.ts
@@ -1,173 +1,183 @@
 export enum ELoggerLevel {
-    DEBUG,
-    INFO,
-    WARN,
-    ERROR,
-    FATAL
-};
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+  FATAL
+}
 
 const MAX_COL = 30;
 
 export const MLogLevel = new Map<ELoggerLevel, string>([
-        [ELoggerLevel.DEBUG, 'DEBUG'],
-        [ELoggerLevel.INFO, 'INFO'],
-        [ELoggerLevel.WARN, 'WARN'],
-        [ELoggerLevel.ERROR, 'ERROR'] ,
-        [ELoggerLevel.FATAL, 'FATAL']
+  [ELoggerLevel.DEBUG, "DEBUG"],
+  [ELoggerLevel.INFO, "INFO"],
+  [ELoggerLevel.WARN, "WARN"],
+  [ELoggerLevel.ERROR, "ERROR"],
+  [ELoggerLevel.FATAL, "FATAL"]
 ]);
 
 export type LoggerConfigurationItem = {
-    level : ELoggerLevel,
-    appenders : Array<(message: string) => void>
+  appenders: Array<(message: string) => void>;
+  level: ELoggerLevel;
 };
 
 export abstract class Logger {
-    protected configuration: { [key : string] : LoggerConfigurationItem } = {};
+  protected configuration: { [key: string]: LoggerConfigurationItem } = {};
 
-    /**
-     * constructor of Logger
-     * protected to prevent direct
-     */
-    protected constructor() {
-        // default constructor protected for singleton pattern
-    }
+  /**
+   * constructor of Logger
+   * protected to prevent direct
+   */
+  protected constructor() {
+    // default constructor protected for singleton pattern
+  }
 
-    /**
-     * method to print message
-     * @param theme the theme associated of log
-     * @param messages the liste of message to log
-     */
-    public debug(theme: string, ...messages : string[]) : void {
-        this.trace(ELoggerLevel.DEBUG, theme, messages);
-    }
-    /**
-     * method to print message
-     * @param theme the theme associated of log
-     * @param messages the liste of message to log
-     */
-    public info(theme: string, ...messages : string[]) : void {
-        this.trace(ELoggerLevel.INFO, theme, messages);
-    }
-    /**
-     * method to print message
-     * @param theme the theme associated of log
-     * @param messages the liste of message to log
-     */
-    public warn(theme: string, ...messages : string[]) : void {
-        this.trace(ELoggerLevel.WARN, theme, messages);
-    }
-    /**
-     * method to print message
-     * @param theme the theme associated of log
-     * @param message the message to log
-     * @param error the exception to log
-     */
-    public error(theme: string, message : string, error : Error) : void{
-        this.trace(ELoggerLevel.ERROR, theme, [message], error);
-    }
-    /**
-     * method to print message
-     * @param theme the theme associated of log
-     * @param message the message to log
-     * @param error the exception to log
-     */
-    public fatal(theme: string, message : string, error : Error) : void {
-        this.trace(ELoggerLevel.FATAL, theme, [message], error);
-    }
+  /**
+   * method to print message
+   * @param theme the theme associated of log
+   * @param messages the liste of message to log
+   */
+  public debug(theme: string, ...messages: string[]): void {
+    this.trace(ELoggerLevel.DEBUG, theme, messages);
+  }
+  /**
+   * method to print message
+   * @param theme the theme associated of log
+   * @param messages the liste of message to log
+   */
+  public info(theme: string, ...messages: string[]): void {
+    this.trace(ELoggerLevel.INFO, theme, messages);
+  }
+  /**
+   * method to print message
+   * @param theme the theme associated of log
+   * @param messages the liste of message to log
+   */
+  public warn(theme: string, ...messages: string[]): void {
+    this.trace(ELoggerLevel.WARN, theme, messages);
+  }
+  /**
+   * method to print message
+   * @param theme the theme associated of log
+   * @param message the message to log
+   * @param error the exception to log
+   */
+  public error(theme: string, message: string, error: Error): void {
+    this.trace(ELoggerLevel.ERROR, theme, [message], error);
+  }
+  /**
+   * method to print message
+   * @param theme the theme associated of log
+   * @param message the message to log
+   * @param error the exception to log
+   */
+  public fatal(theme: string, message: string, error: Error): void {
+    this.trace(ELoggerLevel.FATAL, theme, [message], error);
+  }
 
-    /**
-     * method to print message
-     * @param theme the theme associated of log
-     * @param messages the list of message to log
-     * @param error the exception to log
-     */
-    public trace(level: ELoggerLevel, theme: string, messages : string[], error ?: Error) : void {
-        if (level >= this.configuration[theme].level) {
-            const message = this.getMessage(level, theme, messages, error);
-            this.configuration[theme].appenders.forEach((appender) => appender(message));
-        }
+  /**
+   * method to print message
+   * @param theme the theme associated of log
+   * @param messages the list of message to log
+   * @param error the exception to log
+   */
+  public trace(
+    level: ELoggerLevel,
+    theme: string,
+    messages: string[],
+    error?: Error
+  ): void {
+    if (level >= this.configuration[theme].level) {
+      const message = this.getMessage(level, theme, messages, error);
+      this.configuration[theme].appenders.forEach((appender) =>
+        appender(message)
+      );
     }
-     /**
-     * method to format message with error to log
-     * @param level the level of log
-     * @param theme the theme associated of log
-     * @param messages the list of message to log
-     * @param error the exception to log
-     * @return the formated log
-     */
-    public abstract getMessage(level : ELoggerLevel, theme : string, messages : string[], error ?: Error) : string;
+  }
+  /**
+   * method to format message with error to log
+   * @param level the level of log
+   * @param theme the theme associated of log
+   * @param messages the list of message to log
+   * @param error the exception to log
+   * @return the formated log
+   */
+  public abstract getMessage(
+    level: ELoggerLevel,
+    theme: string,
+    messages: string[],
+    error?: Error
+  ): string;
 }
 
+import path from "path";
+import { promises as fs } from "fs";
 
-
-
-import path from 'path';
-import { promises as fs } from 'fs';
-
-export const LoggerTheme : {[key: string] : string} = {
-    IO : 'IO',
-    DATA : 'DATA'
+export const LoggerTheme: { [key: string]: string } = {
+  IO: "IO",
+  DATA: "DATA"
 };
 
-
 export class CliLogger extends Logger {
-    private static batchLog : string[] = [];
+  private static batchLog: string[] = [];
 
-    private static logger: CliLogger | null = null;
-    /**
-     * static method access to the singleton instance.
-     */
-    public static getInstance(): CliLogger {
-        if (!CliLogger.logger) {
-            CliLogger.logger = new CliLogger();
-        }
-        return CliLogger.logger;
+  private static logger: CliLogger | null = null;
+  /**
+   * static method access to the singleton instance.
+   */
+  public static getInstance(): CliLogger {
+    if (!CliLogger.logger) {
+      CliLogger.logger = new CliLogger();
     }
+    return CliLogger.logger;
+  }
 
-    constructor () {
-        super();
-        this.configuration = {
-            IO : {
-                level : ELoggerLevel.INFO,
-                appenders : [
-                    console.info,
-                    this.write
-                ]
-            },
-            DATA : {
-                level : ELoggerLevel.INFO,
-                appenders : [
-                    console.info,
-                    this.write
-                ]
-            }
-        };
+  constructor() {
+    super();
+    this.configuration = {
+      IO: {
+        level: ELoggerLevel.INFO,
+        appenders: [console.info, this.write]
+      },
+      DATA: {
+        level: ELoggerLevel.INFO,
+        appenders: [console.info, this.write]
+      }
+    };
+  }
+  private write(message: string): void {
+    if (CliLogger.batchLog.length < MAX_COL) {
+      CliLogger.batchLog.push(message);
+    } else {
+      fs.writeFile(
+        path.resolve(process.cwd(), "audiso.log"),
+        `${CliLogger.batchLog.join("\n")}\n`,
+        { flag: "a" }
+      );
+      CliLogger.batchLog = [];
     }
-        private write(message : string) : void {
-            if (CliLogger.batchLog.length < MAX_COL) {
-                CliLogger.batchLog.push(message);
-            } else {
-                fs.writeFile(path.resolve(process.cwd(), 'audiso.log'), `${CliLogger.batchLog.join('\n')}\n`, { flag : 'a' });
-                CliLogger.batchLog = [];
-            }
-        }
+  }
 
-       public getMessage(level : ELoggerLevel, theme : string, messages : string[], error ?: Error) : string {
-            return `${(new Date()).toLocaleString('fr-FR')} [${MLogLevel.get(level)}] - ${theme} : ${messages.join(' ')} ${error?.message} - stack : ${error?.stack}`;
-        }
+  public getMessage(
+    level: ELoggerLevel,
+    theme: string,
+    messages: string[],
+    error?: Error
+  ): string {
+    return `${new Date().toLocaleString("fr-FR")} [${MLogLevel.get(
+      level
+    )}] - ${theme} : ${messages.join(" ")} ${error?.message} - stack : ${
+      error?.stack
+    }`;
+  }
 
-        public stopLogger() : void {
-            fs.writeFile(path.resolve(process.cwd(), 'audiso.log'), `${CliLogger.batchLog.join('\n')}\n`, { flag : 'a' });
-            CliLogger.batchLog = [];
-            CliLogger.logger = null;
-        }
-}
-
-/**
- * function to get have safe error from catch block argument
- * @param error the error to check
- * @param defaultValue the error if first argument is not valid
- */
-export function getError(error : unknown, defaultValue : string) : Error {
-    return error instanceof Error ? error : Error(defaultValue);
+  public stopLogger(): void {
+    fs.writeFile(
+      path.resolve(process.cwd(), "audiso.log"),
+      `${CliLogger.batchLog.join("\n")}\n`,
+      { flag: "a" }
+    );
+    CliLogger.batchLog = [];
+    CliLogger.logger = null;
+  }
 }
